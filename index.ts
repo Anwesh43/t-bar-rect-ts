988a35b79485c9f219e46765dba46266b17a0b44
Anwesh43 <anweshthecool0@gmail.com>
2021-06-02 10:55:36 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,40 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawTBarRect(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const size : number = Math.min(w, h) / sizeFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        DrawingUtil.drawLine(context, 0, size, 0, size - 2 * size * sf1)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawLine(context, 0, -size, -size * 0.5 * sf2, -size)
+            context.fillRect(-size / 2, -size, size / 2, 2 * size * sf3)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawTBRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawTBarRect(context, scale)
+    }
+}
